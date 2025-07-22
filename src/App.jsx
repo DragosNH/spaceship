7e83b1d4f7461e0fd18d359e4d32f0864ce47446
Dragoş <edragos33@gmail.com>
2025-07-22 17:05:04 +0200

    added orbit controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import './App.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 function App() {
 
   // Scene added
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 
   // Renderer
   const renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
   camera.position.z = 5;
 
+  const controls = new OrbitControls(camera, renderer.domElement);
 
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
 
 
   // Responsive design
@@ -33,9 +38,12 @@ function App() {
   });
 
   function animate() {
-    renderer.render( scene, camera );
+
+    controls.update();
+
+    renderer.render(scene, camera);
   }
-  renderer.setAnimationLoop( animate );
+  renderer.setAnimationLoop(animate);
 
 }
 
